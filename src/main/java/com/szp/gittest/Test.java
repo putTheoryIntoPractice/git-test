eb4cf03b7f326a42b1bbec2818199a6d98de50c9
songzhipeng <songzhipeng@hupu.com>
2019-12-19 10:10:45 +0800

    test

---
+++ src/main/java/com/szp/gittest/Test.java
@@ -0,0 +1,11 @@
+package com.szp.gittest;
+
+/**
+ * @Author Song Zhipeng
+ * @Date 2019/12/19
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
